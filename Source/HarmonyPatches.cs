c6fa83acbd2a3b410532e501e8b45d1d5d71726d
jecrell <matt.walls31@gmail.com>
2018-01-10 22:16:39 +0900

    Added assembly foundations

---
+++ Source/HarmonyPatches.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Harmony;
+using ProjectHeron;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Duskilor
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+
+        static HarmonyPatches()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("rimworld.jecrell.duskilor");
+//            harmony.Patch(AccessTools.Method(typeof(Building_Door), "DoorOpen"), new HarmonyMethod(typeof(HarmonyPatches),
+//                nameof(InvisDoorOpen)), null);
+        }
+    }
+}
+
